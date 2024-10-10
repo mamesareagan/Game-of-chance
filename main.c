2b9365df3610405acf2ca8072c0ef98bd68b3619
Reagan <mamesareagan0@gmail.com>
2024-10-10 20:52:21 +0300

    update player data for evry game

--- main.c
+++ main.c
@@ -5,12 +5,12 @@ User player;
 
 int main(void)
 {
-	int choice = 0;//, last_game;
+	int choice = 0, last_game = 0;
 
 	srand(time(0));	/*seed the randomizer with the current time*/
 	if (get_player_data() == -1)
 		register_new_player();
-	
+	printf("[DEBUG] Address of pick_a_number: %p\n", (void*)pick_a_number);
 	while (choice != 7)
 	{
 		printf("-~~~[Game of Chance Menu]~~~-\n");
@@ -32,6 +32,24 @@ int main(void)
 
 		if ((choice < 1) || (choice > 7))
 			printf("\n[!!] The number %d is an invalid selection.\n\n", choice);
-	}		
+		else if (choice < 4)
+		{
+			if(choice != last_game)	//if the function ptr in the struct isnt set yet
+			{
+				if (choice == 1)
+				{
+					printf("[DEBUG] Address of pick_a_number: %p\n", (void*)pick_a_number);
+					player.current_game = &pick_a_number;
+					printf("Current game function pointer: %p\n", (void*)player.current_game);
 
+					last_game = choice;
+				}
+			}
+			play_the_game();
+			update_player_data();
+		}
+		
+	}
+	update_player_data();
+	printf("\nThanks for playing! Bye.\n");
 }
